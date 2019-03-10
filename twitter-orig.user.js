77957c07925883c7e34030aa67c8f25a8a79bf41
horyu <horyu@users.noreply.github.com>
2019-03-10 11:41:32 +0900

    add semicolons

--- twitter-orig.user.js
+++ twitter-orig.user.js
@@ -42,7 +42,7 @@ function mutationObserverCallback(mutations) {
 				if(node.nodeType != Node.ELEMENT_NODE)
 					continue;
 
-				onAddedNode(node)
+				onAddedNode(node);
 				for(let subNode of node.querySelectorAll(TweetVideoSelector))
 					onAddedNode(subNode);
 				for(let subNode of node.querySelectorAll(TweetImageSelector))
@@ -50,7 +50,7 @@ function mutationObserverCallback(mutations) {
 			}
 		}
 	} catch(e) {
-		console.log(e)
+		console.log(e);
 	}
 }
 
@@ -58,30 +58,30 @@ function visitOnce(element, func) {
 	if(alreadyVisited.has(element))
 		return;
 	alreadyVisited.add(element);
-	func()
+	func();
 }
 
 function onAddedNode(node) {
 	if(node.matches(TweetImageSelector)) {
 		visitOnce(node, () => {
 			addImageControls(node.closest(".tweet, .Tweet, .js-stream-item-content"),node);
-		})
+		});
 	}
 
 	if(node.matches(TweetVideoSelector)) {
-		// we match an iframe here. once on the parent because iframes get reloaded when scrolling
+		// we match an iframe here.once on the parent because iframes get reloaded when scrolling
 		visitOnce(node.parentElement, () => {
-			addVideoControls(node.closest(".tweet"), node)
-		})
+			addVideoControls(node.closest(".tweet"), node);
+		});
 	}
 }
 
 function controlContainer(target) {
 	let div = target.querySelector(`.${cssPrefix}-thumbs-container`);
 	if(!div) {
-		div = document.createElement("div")
-		target.appendChild(div)
-		div.className = prefixed("-thumbs-container")
+		div = document.createElement("div");
+		target.appendChild(div);
+		div.className = prefixed("-thumbs-container");
 	}
 
 	return div;
@@ -95,12 +95,12 @@ function addImageControls(tweetContainer, image) {
 		src = image.src;
 	}
 
-	let origSrc = src + ":orig"
+	let origSrc = src + ":orig";
 	let div = controlContainer(tweetContainer);
 
 	div.insertAdjacentHTML("beforeend", `
 		<a class="${cssPrefix}-orig-link ${cssPrefix}-thumb" data-${cssPrefix}-small="${src}" href="${origSrc}"><img src="${src}"></a>
-	`)
+	`);
 }
 
 const supportedContentTypes = [
@@ -122,7 +122,7 @@ const supportedContentTypes = [
 		ext: "mp4",
 		loader: fetchVmap
 	}
-]
+];
 
 // can't use fetch() API here since it's blocked by CSP
 function fetchVmap(configPromise) {
@@ -133,10 +133,10 @@ function fetchVmap(configPromise) {
 				url: config.vmap_url,
 				responseType: "xml",
 				anonymous: true,
-				onload: (rsp) => { resolve(rsp.responseXML) },
-				onerror: (e) => { reject(e) }
-			})
-		})
+				onload: (rsp) => { resolve(rsp.responseXML); },
+				onerror: (e) => { reject(e); }
+			});
+		});
 	}).then(xmlDoc => {
 		let url = xmlDoc.querySelector("*|MediaFile").textContent;
 		return new Promise((resolve, reject) => {
@@ -145,48 +145,48 @@ function fetchVmap(configPromise) {
 				url: url,
 				responseType: "blob",
 				anonymous: true,
-				onload: (rsp) => { resolve(rsp.response) },
-				onerror: (e) => { reject(e) }
-			})
-		})
-	})
+				onload: (rsp) => { resolve(rsp.response); },
+				onerror: (e) => { reject(e); }
+			});
+		});
+	});
 }
 
 function fetchMpegTs(configPromise) {
 	let baseURL = null;
 
 	return configPromise.then(config => {
-		baseURL = config.video_url
+		baseURL = config.video_url;
 
 		return fetch(config.video_url, {redirect: "follow", mode: "cors"}).then((response) => {
-			return response.text()
-		})
+			return response.text();
+		});
 	}).then((playlist) => {
-		let highestResolution = playlist.split(/\n/).filter(str => !str.startsWith("#")).filter(str => str.length > 0).pop()
-		let fetchUrl = new URL(baseURL)
-		fetchUrl.pathname = highestResolution
+		let highestResolution = playlist.split(/\n/).filter(str => !str.startsWith("#")).filter(str => str.length > 0).pop();
+		let fetchUrl = new URL(baseURL);
+		fetchUrl.pathname = highestResolution;
 		return fetch(fetchUrl, {mode: "cors", redirects: "follow"});
 	}).then((response) => {
-		return response.text()
+		return response.text();
 	}).then((chunkList) => {
 		return chunkList.split(/\n/).filter(s => !s.startsWith("#")).filter(s => s.length > 0).map(chunk => {
 			let u = new URL(baseURL);
 			u.pathname = chunk;
 			return u;
-		})
+		});
 	}).then((urls) => {
 		return Promise.all(urls.map(u => {
-			return fetch(u.toString(), {mode: "cors", redirects: "follow"}).then(response => response.blob())
+			return fetch(u.toString(), {mode: "cors", redirects: "follow"}).then(response => response.blob());
 		}));
 	}).then(blobs => {
 			return new Blob(blobs);
-	})
+	});
 }
 
 function fetchMP4(configPromise) {
 	return configPromise.then(config => {
-		return fetch(config.video_url, {redirect: "follow", mode: "cors"}).then(response => response.blob())
-	})
+		return fetch(config.video_url, {redirect: "follow", mode: "cors"}).then(response => response.blob());
+	});
 }
 
 
@@ -195,29 +195,29 @@ function addVideoControls(tweetContainer, iframe) {
 
 	let configPromise = new Promise((resolve, reject) => {
 		if(iframe.contentDocument.readyState == "interactive" || iframe.contentDocument.readyState == "complete") {
-			resolve(iframe.contentDocument)
+			resolve(iframe.contentDocument);
 			return;
 		}
 
-		iframe.addEventListener("load", () => resolve(iframe.contentDocument))
+		iframe.addEventListener("load", () => resolve(iframe.contentDocument));
 	}).then((contentDoc) => {
-		let config = JSON.parse(contentDoc.querySelector(".player-container").dataset.config)
+		let config = JSON.parse(contentDoc.querySelector(".player-container").dataset.config);
 
 		mediaConfig = config;
 
-		console.log(config)
+		console.log(config);
 
 		if(!supportedContentTypes.find(t => t.matcher(config)))
 			throw new Error(`unknown video configuration, unable to fetch data`);
 
-		return config
-	})
+		return config;
+	});
 
-	const controls = controlContainer(tweetContainer)
+	const controls = controlContainer(tweetContainer);
 
 	controls.insertAdjacentHTML("beforeend", `
 		<a download="${Date.now()}.ts" href="#">download</a><span class="${cssPrefix}-progress"></span>
-	`)
+	`);
 
 	let finalBlob = null;
 	const link = controls.querySelector("a[download]");
@@ -229,19 +229,19 @@ function addVideoControls(tweetContainer, iframe) {
 					${message}:
 					${exception.toString()}
 				</span>
-			`)
-		}
-	}
+			`);
+		};
+	};
 
-	configPromise.catch(exceptionHandler("An error occured while reading the video metadata"))
+	configPromise.catch(exceptionHandler("An error occured while reading the video metadata"));
 
 	configPromise.then(config => {
-		const type = supportedContentTypes.find(t => t.matcher(config))
+		const type = supportedContentTypes.find(t => t.matcher(config));
 
-		let filename = `@${config.user.screen_name} ${config.tweet_id}.${type.ext}`
+		let filename = `@${config.user.screen_name} ${config.tweet_id}.${type.ext}`;
 		link.download = filename;
-		link.appendChild(document.createTextNode(": " + filename))
-	})
+		link.appendChild(document.createTextNode(": " + filename));
+	});
 
 
 	link.addEventListener("click", (e) => {
@@ -251,8 +251,8 @@ function addVideoControls(tweetContainer, iframe) {
 		e.preventDefault();
 
 		configPromise.then(config => {
-			const type = supportedContentTypes.find(t => t.matcher(config))
-			return type.loader(configPromise)
+			const type = supportedContentTypes.find(t => t.matcher(config));
+			return type.loader(configPromise);
 
 		}).then(blob => {
 			finalBlob = blob;
@@ -261,12 +261,12 @@ function addVideoControls(tweetContainer, iframe) {
 
 			// fire new click event since we prevent-defaulted it earlier
 			link.click();
-		}).catch(exceptionHandler("An error occurred while downloading the video"))
+		}).catch(exceptionHandler("An error occurred while downloading the video"));
 
-	})
+	});
 }
 
-let observer = null
+let observer = null;
 
 function init() {
 	const config = { subtree: true, childList: true };
@@ -274,9 +274,9 @@ function init() {
 	observer = new MutationObserver(mutationObserverCallback);
 	observer.observe(document.documentElement, config);
 
-	document.addEventListener("DOMContentLoaded", ready)
-	document.addEventListener("click", thumbToggleHandler, true)
-	document.addEventListener("keypress", keyboardNav)
+	document.addEventListener("DOMContentLoaded", ready);
+	document.addEventListener("click", thumbToggleHandler, true);
+	document.addEventListener("keypress", keyboardNav);
 
 }
 
@@ -290,7 +290,7 @@ function thumbToggleHandler(event) {
 	event.stopImmediatePropagation();
 	event.preventDefault();
 
-	thumbToggle(link)
+	thumbToggle(link);
 }
 
 
@@ -300,22 +300,22 @@ function thumbToggle(link) {
 	return new Promise((res, rej) => {
   	if(link.classList.contains(prefixed("-expanded"))) {
 			img.src = link.dataset[cssPrefix + "Small"];
-			link.classList.add(prefixed("-thumb"))
-			link.classList.remove(prefixed("-expanded"))
-			res(link)
+			link.classList.add(prefixed("-thumb"));
+			link.classList.remove(prefixed("-expan;ded"));
+			res(link);
 		} else {
 			let f = () => {
-				link.classList.add(prefixed("-expanded"))
-				link.classList.remove(prefixed("-thumb"))
-				img.removeEventListener("load", f)
-				res(link)
-			}
+				link.classList.add(prefixed("-expanded"));
+				link.classList.remove(prefixed("-thumb"));
+				img.removeEventListener("load", f);
+				res(link);
+			};
 
-			img.addEventListener("load", f)
+			img.addEventListener("load", f);
 			img.src = link.href;
 		}
 
-	})
+	});
 }
 
 const style = `
@@ -374,7 +374,7 @@ function ready() {
 	let styleEl = document.createElement("style");
 	styleEl.textContent = style;
 	document.head.append(styleEl);
-	document.querySelector(".ProfileSidebar").insertAdjacentHTML("beforeend", info)
+	document.querySelector(".ProfileSidebar").insertAdjacentHTML("beforeend", info);
 }
 
 function keyboardNav(e) {
@@ -396,31 +396,31 @@ function keyboardNav(e) {
 
 	if(e.key == "q" || e.key == " ") {
 		let cf = currentFocus();
-		let expandable = cf && Array.from(cf.querySelectorAll("." + prefixed("-thumb"))) || []
-		let first = expandable.map((ex) => thumbToggle(ex)).shift()
+		let expandable = cf && Array.from(cf.querySelectorAll("." + prefixed("-thumb"))) || [];
+		let first = expandable.map((ex) => thumbToggle(ex)).shift();
 		if(first)
 			first.then((f) => {
-				setFocus(f, cf)
+				setFocus(f, cf);
 			});
 		prevent = true;
 	}
 
 	if(focus) {
-		setFocus(focus)
+		setFocus(focus);
 	}
 
 	if (e.key == "e") {
 		let cf = currentFocus();
 		if(!cf)
 			return;
-		let config = cf.closest(".tweet").dataset
+		let config = cf.closest(".tweet").dataset;
 		let todownload = [];
-		if(cf.matches("." + prefixed("-expanded")))
+		if(cf.matches("." + prefixed("-expanded")));
 			 todownload.push(cf.href);
-		todownload.push(...Array.from(cf.querySelectorAll("a." + prefixed("-orig-link"))).map((el) => el.href))
+		todownload.push(...Array.from(cf.querySelectorAll("a." + prefixed("-orig-link"))).map((el) => el.href));
 
 		for(let link of todownload) {
-			downloadOrig(link, config)
+			downloadOrig(link, config);
 		}
 		prevent = true;
 	}
@@ -431,31 +431,31 @@ function keyboardNav(e) {
 
 function downloadOrig(url, meta) {
 	fetch(url, {redirect: "follow", mode: "cors"}).then(response => response.blob()).then((blob) => {
-		const a = document.createElement("a")
+		const a = document.createElement("a");
 		const blobUri = URL.createObjectURL(blob);
-		a.href = blobUri
+		a.href = blobUri;
 
-		let name = url.match(/^.*\/(.*?):orig$/)[1]
-		a.download = `@${meta.screenName} ${meta.tweetId} orig ${name}`
-		const event = document.createEvent("MouseEvents")
+		let name = url.match(/^.*\/(.*?):orig$/)[1];
+		a.download = `@${meta.screenName} ${meta.tweetId} orig ${name}`;
+		const event = document.createEvent("MouseEvents");
 		event.initMouseEvent(
 			"click", true, false, window, 0, 0, 0, 0, 0,
 			false, false, false, false, 0, null
-		)
-		a.dispatchEvent(event)
-	})
+		);
+		a.dispatchEvent(event);
+	});
 }
 
 function setFocus(focus, expect) {
-	let cf = currentFocus()
+	let cf = currentFocus();
 	if(expect && cf != expect)
 		return;
 	if(cf)
 		cf.classList.remove(prefixed("-focused"));
-	focus.classList.add(prefixed("-focused"))
-	focus.scrollIntoView()
+	focus.classList.add(prefixed("-focused"));
+	focus.scrollIntoView();
 	let offset = document.querySelector(".ProfileCanopy-inner");
-	offset = offset && offset.scrollHeight
+	offset = offset && offset.scrollHeight;
 	if(offset) {
 		offset = offset + 5;
 		window.scrollBy(0, -offset);
@@ -464,7 +464,7 @@ function setFocus(focus, expect) {
 }
 
 function currentFocus() {
-	return document.querySelector(`.${prefixed("-focused")}`)
+	return document.querySelector(`.${prefixed("-focused")}`);
 }
 
 function mod(n, m) {
@@ -474,16 +474,16 @@ function mod(n, m) {
 function moveFocus(direction) {
 	// TODO: mobile, tweetdeck
 
-	let focusable = Array.from(document.querySelectorAll(`.tweet.has-content, .${prefixed("-expanded")}`))
-	let idx = -1
-	let cf = currentFocus()
+	let focusable = Array.from(document.querySelectorAll(`.tweet.has-content, .${prefixed("-expanded")}`));
+	let idx = -1;
+	let cf = currentFocus();
 	if(cf)
 		idx = focusable.indexOf(cf);
 	idx += direction;
-	idx = mod(idx, focusable.length)
-	let newFocus = focusable[idx]
+	idx = mod(idx, focusable.length);
+	let newFocus = focusable[idx];
 
-	return newFocus
+	return newFocus;
 }
 
 
